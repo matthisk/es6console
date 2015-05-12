c4803c5551f9b7e25dfe84dc7ddbbb220a604685
Matthisk Heimensen <matthiskheimensen@gmail.com>
2015-05-12 10:01:46 +0200

    Initial import

---
+++ index.js
@@ -0,0 +1,55 @@
+var jsMode = require('codemirror/mode/javascript/javascript')
+var CodeMirror = require('codemirror');
+
+(function() {
+  var inputTextArea = document.getElementById("input");
+  var outputTextArea = document.getElementById("output");
+  var compilerSelect = document.getElementById("compiler");
+  var fiddleConsole = document.getElementById("console");
+
+  var config = {
+    mode : "javascript",
+    lineNumbers : true
+  };
+
+  var outputEditor = CodeMirror.fromTextArea(outputTextArea, config);
+  var inputEditor = CodeMirror.fromTextArea(inputTextArea, config);
+
+  var Babel = (function() {
+    return {
+      compile : function( input ) {
+        return babel.transform( input ).code;
+      }
+    };
+  })();
+
+  var Traceur = (function() {
+    this.compiler = new traceur.Compiler();
+
+    return {
+      compile : function( input ) {
+        return this.compiler.compile( input );
+      }
+    };
+  })();
+
+  var Compilers = {
+    'Babel' : Babel,
+    'Traceur' : Traceur
+  };
+
+  var transformer = function( input, output, transform ) {
+    output( transform( input() ) );
+  };
+
+  document.getElementById("transform").onclick = function() { 
+    transformer( inputEditor.getValue.bind(inputEditor), outputEditor.setValue.bind(outputEditor), Compilers[ compilerSelect.value ].compile ); 
+  };
+
+  document.getElementById("run").onclick = function() {
+    var code = outputEditor.getValue();
+    var result = eval( code );
+
+    fiddleConsole.value += "> " + result.toString() + "\n";
+  };
+})();
