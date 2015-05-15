035fd70f67422eb61bcff2b86fcb699f6ddf61e9
Matthisk Heimensen <matthiskheimensen@gmail.com>
2015-05-15 17:19:00 +0200

    Eval is ran in a sandbox, better console.log support

--- js/index.js
+++ js/index.js
@@ -16,16 +16,43 @@ var setup = function( wr ) {
   setup( wrapper );
 
   var cnsl = new Console();
-
-  window.console.log = function( msg ) {
-    cnsl.writeLine( msg );
-  }
-
+  
+  // DOM Elements
   var inputTextArea = $("#input"),
       outputTextArea = $("#output"),
       compilerSelect = $("#compiler"),
       runBtn = $("#run"),
-      transformBtn = $("#transform");
+      transformBtn = $("#transform"),
+      body = document.getElementsByTagName("body")[0],
+      sandboxFrame = document.createElement("iframe");
+
+  body.appendChild( sandboxFrame );
+
+  // Overwrite the console log function of the sandbox to write results
+  // to our own console.
+  sandboxFrame.contentWindow.console.log = function( ...args ) {
+    let res = '';
+    if( typeof args[0] === 'string' && args[0].search("%s") !== -1) {
+      let s = args[0],
+          search,
+          i = 1;
+
+      while( i < args.length && (search = s.search(/%s|%d|%i|%f]/)) !== -1 ) {
+        let replace = s[search] + s[search+1];
+        s = s.replace(replace,args[i]);
+        i++;
+      }
+
+      cnsl.writeLine( s );
+    } else {
+      args.forEach(arg => {
+        if( typeof arg === 'object' ) res += `${JSON.stringify(arg)} `;
+        else res += `${arg} `;
+      });
+    }
+
+    cnsl.writeLine( res ); 
+  };
 
   var [inputEditor,outputEditor] = Editors.create( inputTextArea, outputTextArea );
   inputEditor.focus();
@@ -33,7 +60,7 @@ var setup = function( wr ) {
   var runCode = function( code ) {
     let result;
     try {
-      result = eval( code );
+      result = sandboxFrame.contentWindow.eval( code );
     } catch( e ) {
       return cnsl.writeError( e );
     }
